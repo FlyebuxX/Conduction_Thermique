40595ca019d1ed3e5a3cc8483530742f9f29e4ff
Valentin <valentin.eberhardt57@gmail.com>
2025-03-03 15:37:48 +0100

    Projet de Base

---
+++ ProjetDeBase/src/IHM/FenetrePrincipale.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IHM;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+/**** @author Christian Daul  */
+public class FenetrePrincipale extends JFrame {
+    private PanneauPrincipal panPrincipal ;
+    
+    public FenetrePrincipale() {
+        super() ;
+        
+        this.setTitle("TITRE DU PROGRAMME");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocation(400, 300);
+        this.setMinimumSize(new Dimension(650,480));
+        
+        this.panPrincipal = new PanneauPrincipal() ;
+        this.add(this.panPrincipal);
+        
+        this.pack();
+        this.setVisible(true);
+    }   // Fin du constructeur    
+} // Fin de la classe FenetrePrincipale
---
+++ ProjetDeBase/src/IHM/PanneauPrincipal.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IHM;
+
+
+
+import javax.swing.JPanel;
+
+/**** @author Christian Daul */
+public class PanneauPrincipal extends JPanel {
+    
+    
+     /****************************************************
+     *  LE CONSTRUCTEUR DU PANNEAU PRINCIPAL 
+     ****************************************************/
+    
+    public PanneauPrincipal() {
+        super();
+       
+        
+        
+
+    } // Fin du constructeur sans paramètre  
+    
+    
+    
+} // Fin de la classe PanneauPrincipal
